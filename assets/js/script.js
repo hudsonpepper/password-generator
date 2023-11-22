85be8fb3aeac6b60f61648fcb9ca89d36ac4706c
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2023-11-22 09:50:43 -0500

    Did some messing around

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,20 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("generate password triggered");
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  console.log("Event Triggered!");
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
